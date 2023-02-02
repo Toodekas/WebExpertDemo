2a50ee25a25e8c6ff4389b60b398f875e11788ff
Martin Toode <martin.toode@catapultlabs.eu>
2023-02-02 19:51:47 +0200

    details page css to make it prettier and a back button

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from '../data.service';
 import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-details',
@@ -11,12 +12,20 @@ export class DetailsComponent implements OnInit {
 
   user$: any;
 
-  constructor(private apiClient: DataService, private route: ActivatedRoute) {
+  constructor(
+    private apiClient: DataService,
+    private route: ActivatedRoute,
+    private location: Location
+  ) {
     this.route.params.subscribe( params => this.user$ = params['id'])
   }
 
   async ngOnInit() {
     this.user$ = await this.apiClient.getUser(this.user$)
+  }
+
+  goBack(): void {
+    this.location.back();
   }
 
 }
